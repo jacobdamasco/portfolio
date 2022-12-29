1c2e19ce9f5c3118a340d31ce192897ab2dcef93
jrdamasco <jdamasco616@gmail.com>
2022-12-29 02:01:27 -0800

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <>
+        <div className="flex flex-row bg-black max-w-screen p-4">
+            <div className="basis-1/12 justify-center text-center"><ul>JD</ul></div>
+            <div className="basis-2/3"></div>
+            <div className="basis-1/12 justify-center text-center"><ul>About</ul></div>
+            <div className="basis-1/12 justify-center text-center"><ul>Projects</ul></div>
+            <div className="basis-1/12 justify-center text-center"><ul>Contact</ul></div>
+        </div>
+      </>
+    );
+}
+
+export default Navbar;
---
+++ src/components/TitlePage.js
@@ -0,0 +1,18 @@
+const TitlePage = () => {
+    return (
+        <>
+        <div className="flex flex-row bg-off-black-1 py-60 px-4">
+            <div className="basis-1/2 px-12">
+                <p>Hi, I'm</p>
+                <p>Jacob Damasco</p>
+                <p>I'm a computer engineering/computer science major at USC with a passion for engineering. </p>
+            </div>
+            <div className="basis-1/2">
+                
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default TitlePage;
